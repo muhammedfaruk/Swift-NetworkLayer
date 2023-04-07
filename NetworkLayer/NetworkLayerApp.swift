0cca386ce600328ee7df6022abe7a94a9da797ea
Muhammed Faruk <faruk.sogut@btpro.net>
2023-04-07 22:56:44 +0300

    Initial Commit

---
+++ NetworkLayer/NetworkLayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkLayerApp.swift
+//  NetworkLayer
+//
+//  Created by Muhammed Faruk Söğüt on 7.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkLayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
